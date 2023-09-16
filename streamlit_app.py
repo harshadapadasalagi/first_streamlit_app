f55cca01978f9fd7c6ce5369bbac4fd615490453
harshadapadasalagi <144982063+harshadapadasalagi@users.noreply.github.com>
2023-09-16 16:54:10 +0530

    Update streamlit_app.py
    added emoji's

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents New good Helthy Dinner!')
-streamlit.header('Breakfast, Menu')
-streamlit.text('Idly')
-streamlit.text('Wada')
-streamlit.text('Dosa')
+streamlit.header('🥣 Breakfast Favorites')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
